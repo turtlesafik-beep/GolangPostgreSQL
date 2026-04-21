3379f7cd2605f47d44dd97c26a640ccfa058d691
Danila Safronov <tutlesafik@mail.ru>
2026-04-21 16:09:51 +0300

    connect to postgres

--- feature_postgre/simple_connection/simple_connection.go
+++ feature_postgre/simple_connection/simple_connection.go
@@ -1,1 +1,22 @@
 package simple_connection
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/jackc/pgx/v5"
+)
+
+func CheckConnection() {
+	ctx := context.Background()
+	conn, err := pgx.Connect(ctx, "postgres://postgres:0718@localhost:5432/postgres")
+	if err != nil {
+		panic(err)
+	}
+
+	if err := conn.Ping(ctx); err != nil {
+		panic(err)
+	}
+
+	fmt.Println("Great connect to database")
+}
--- main.go
+++ main.go
@@ -1,1 +1,7 @@
 package main
+
+import "GolangPostgres/feature_postgre/simple_connection"
+
+func main() {
+	simple_connection.CheckConnection()
+}
